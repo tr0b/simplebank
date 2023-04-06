e9c411f8f8ae986322e28d37a95b0467dfc1db8c
tr0b <trobinson3000600@gmail.com>
2023-04-06 13:57:28 -0600

    Will add transfer API route and controller logic. Also added a validator for account.currency

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 
 	db "github.com/tr0b/simplebank/db/sqlc"
 )
@@ -15,11 +17,16 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts/", server.listAccounts)
 	router.PATCH("/accounts/:id", server.updateAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
