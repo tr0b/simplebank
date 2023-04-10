145bf112afbf839e7820403bec780eaf316bd422
tr0b <trobinson3000600@gmail.com>
2023-04-09 22:12:38 -0600

    Will remove printing env contents instruction from start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,6 @@ set -e
 
 echo "run db migrations"
 . /app/.env
-echo "printing .env contents"
-less /app/.env
 /app/migrate -path /app/db/migrate -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
